89bca2a2ab582092e63885083dca24a729f0cd32
plokijuuu <magdalenadarlak7@gmail.com>
2025-04-02 01:31:58 +0200

    Uni assigments

---
+++ First Year/C++/długości_ciągów_znaków.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    string x;
+    vector<string> v;
+    cin >> x;
+    while(v.end() != x)
+    {
+
+    }
+    return 0;
+
+}
+
+
